9aa998ca54a2d1ec6b385871c4c58fb7d9bbea0a
Killamsetty <M.Killamsetty@shell.com>
2019-04-24 16:04:52 +0530

    user added

--- scripts/Executables/contacts.js
+++ scripts/Executables/contacts.js
@@ -1,24 +1,83 @@
-
 var addedContacts = []
+var id = document.querySelector('#idField');
+var name = document.querySelector('#nameField');
+var phoneNumber = document.querySelector('#phoneField');
+var imageURL = document.querySelector('#descField');
+var addContactBtn = document.querySelector('#addcontact');
+var deleteContactBtn = document.querySelector('#deleteContact');
+var searchBox = document.querySelector('#searchBox');
+var taskList = document.querySelector('#taskList');
 
-function generateElements(user){
+ 
+
+function generateElements(contact){
+  
+    var li = document.createElement('li');   
+    var image = document.createElement('img');
+    var div1 = document.createElement('div');
+    var div2 = document.createElement('div');
+    var div3 = document.createElement('div');
+    var div4 = document.createElement('div');
+    var img = document.createElement('img');
+    var p1 = document.createElement('p');
+    var p2 = document.createElement('p');
+    var p3 = document.createElement('p');
+    var button = document.createElement('button');
+
+
+    div1.className = 'row card-body';
+    div2.className = 'col';
+    div3.className = 'col m-2';
+    div4.className = 'deleteContact';
+    button.className = 'btn btn-light float-right';
+    img.className = 'img-thumbnail';
+
+
+    div1.appendChild(div2);
+    div1.appendChild(div3);
+    div1.appendChild(div4);
+    div2.appendChild(img);
+    div3.appendChild(p1);
+    div3.appendChild(p2);
+    div3.appendChild(p3);
+    div4.appendChild(button);
+    li.appendChild(div1)
+
+    id.innerText = contact.email
+    name.innerText = contact.name
+    phNumber.innerText = contact.phone
+    image.setAttribute('src',contact.imageURL)
+    
+    return li
 
 }
 
-function reloadView(user){
+function reloadView(users){
         
-    generateElements(user)
+    taskList.innerHTML = ''
+    let assignedUser;
+    users.forEach((element) => {
+           
+        if (element.email == currentUser.email){
+               assignedUser = element
+        }
+    })
+    
+    for (contact of assignedUser.contacts){
+     taskList.appendChild(generateElements(contact));
+    }
+    
 }
 
 
 
-function addContacts(){
+addContactBtn.onclick = function addContacts(){
     var allUsers = fetchAllUsers();
-   let myContact = new Contact('vaishak','9898505884','vaishak94@gmail.com','')
+   let myContact = new Contact(name.value,phoneNumber.value,id.value,imageURL.value)
     addedContacts.push(myContact)
    let currentUser = loggedInUser();
    currentUser.contacts = addContacts
-
+   saveCurrentUser('current',currentUser)
    allUsers.forEach(element => {
        
     if (element.name = currentUser.name){
@@ -42,3 +101,6 @@ function removeContact(index){
     reloadView(fetchAllUsers())
 
 }
+
+
+reloadView(fetchAllUsers())
